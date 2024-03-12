14c3de2698f1b59b84a5a6869e669684045037d1
GabrieleDegioanni <gabriele.degioanni@gmail.com>
2024-03-12 17:00:29 +0100

    Progetto finito

--- main.py
+++ main.py
@@ -23,8 +23,37 @@ class Domanda:
         lista_risposte_mescolate = []
         for j in range(4):
             lista_risposte_mescolate.append(lista_risposte[l[j]])
-        testo = f"{self.testo}{lista_risposte_mescolate[0]}{lista_risposte_mescolate[1]}{lista_risposte_mescolate[2]}{lista_risposte_mescolate[3]}"
+        testo = f"{self.testo}\n{lista_risposte_mescolate[0]}\n{lista_risposte_mescolate[1]}\n{lista_risposte_mescolate[2]}\n{lista_risposte_mescolate[3]}\n"
         return testo
+
+
+class Player:
+    def __init__(self, nome, punteggio):
+        self.nome = nome
+        self.punteggio = punteggio
+
+    def __lt__(self, other):
+        return self.punteggio > other.punteggio
+
+    def __repr__(self):
+        return f"{self.nome} {self.punteggio}"
+
+
+class Game:
+    def __init__(self, lista_domande, giocatore, punteggio):
+        self.lista_domande = seleziona_domande()
+        punti = 0
+        alive = True
+        for i in range(0, len(self.lista_domande)):
+            if alive:
+                if input(self.lista_domande[i]) == self.lista_domande[i].risp_corr:
+                    print("Risposta corretta")
+                    punti += 1
+                else:
+                    print("Risposta errata")
+                    alive = False
+        self.punteggio = punti
+        self.giocatore = Player(input(f"Punteggio raggiunto: {punti}\nInserisci il tuo nickname:\n"), punti)
 
 
 def leggi_domande():
@@ -35,19 +64,19 @@ def leggi_domande():
     i = 1
     for line in file:
         if i == 1:
-            t = line
+            t = line.strip("\n")
         if i == 2:
-            d = int(line)
+            d = int(line.strip("\n"))
         if i == 3:
-            rc = line
+            rc = line.strip("\n")
         if i == 4:
-            r1 = line
+            r1 = line.strip("\n")
         if i == 5:
-            r2 = line
+            r2 = line.strip("\n")
         if i == 6:
-            r3 = line
-            dTemp = Domanda(t, d, rc, r1, r2, r3)
-            domande.add(dTemp)
+            r3 = line.strip("\n")
+            dtemp = Domanda(t, d, rc, r1, r2, r3)
+            domande.add(dtemp)
             i = -1
         i = i + 1
 
@@ -82,10 +111,42 @@ def seleziona_domande():
     return domande
 
 
+def leggi_giocatori():
+    giocatori = set()
+    filename = "punti.txt"
+    file = open(filename, "r")
+
+    for line in file:
+        campi = line.split(" ")
+        gtemp = campi[0]
+        ptemp = int(campi[1].strip("\n"))
+        giocatori.add(Player(gtemp, ptemp))
+
+    file.close()
+    return giocatori
 
 
-domande = seleziona_domande()
-if input(domande[0]) == domande[0].risp_corr.strip("\n"):
-    print("Risposta corretta")
-else:
-    print("Risposta errata")
+def scrivi_giocatori(lista):
+    filename = "punti.txt"
+    file = open(filename, "w")
+
+    for p in lista:
+        file.write(f"{p.nome} {p.punteggio}\n")
+
+    file.close()
+
+
+def main():
+    insieme_giocatori = set()
+    insieme_partite = set()
+    gioco = Game(0, 0, 0)
+    insieme_partite.add(gioco)
+    insieme_giocatori.add(gioco.giocatore)
+    insieme_giocatori_file = leggi_giocatori()
+    insieme_giocatori_fin = insieme_giocatori.union(insieme_giocatori_file)
+    lista = sorted(insieme_giocatori_fin)
+    scrivi_giocatori(lista)
+
+
+if __name__ == "__main__":
+    main()
